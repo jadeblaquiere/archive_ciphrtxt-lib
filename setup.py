5ce4eaf307c74a387c53b53d40314c52d16abec7
jadeblaquiere <jadeblaquiere@yahoo.com>
2016-08-24 11:25:18 -0400

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+from ciphrtxt import __version__
+
+setup(name='ciphrtxt',
+      description='ciphrtxt core library providing definition of core data types',
+      version=str(__version__),
+      author='Joseph deBlaquiere',
+      author_email='jadeblaquiere@yahoo.com',
+      packages=['ciphrtxt'])
